548edb967574327eedcc8d544834ca796c7b5545
Izabel Vidal <izabelvidal99@gmail.com>
2020-10-28 22:15:19 -0300

    controllers e começando models

--- app/Models/cliente.php
+++ app/Models/cliente.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class cliente extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'tipo'
+    ];
+
+    public static $rules = [ 
+        'tipo' => 'required|in:revendedor:consumidor',
+    ];
+
+    public function vendas(){
+        return $this->hasMany('App\Models\Venda');
+    }
+
+    public function pessoa(){
+        return $this->belongsTo('App\Models\Pessoa');
+    }
 }
